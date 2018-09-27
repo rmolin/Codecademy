54e84130171387ec57508623b5d6370678d93b62
Ryan Molin <rbmolin25@gmail.com>
2018-09-27 14:55:17 -0400

    Learn JS: Loops project 1

---
+++ Whale Talk.js
@@ -0,0 +1,22 @@
+const input = 'Hi human, me likes to eat fish';
+const vowels = ['a', 'e', 'i', 'o', 'u'];
+let = resultArray = [];
+
+// for each loop of the outer for loop, you're considering
+// each character
+for (let i = 0; i < input.length; i++){
+  console.log('i is '+ i);
+  for (let j = 0; j < vowels.length; j++){
+		if (input[i] === vowels[j]){
+      resultArray.push(vowels[j]);
+    }
+  }
+    if (input[i] === 'e'){
+      resultArray.push(input[i]);
+    }
+    if (input[i] === 'u'){
+      resultArray.push(input[i]);
+    }
+};
+
+console.log(resultArray.join('').toUpperCase());
